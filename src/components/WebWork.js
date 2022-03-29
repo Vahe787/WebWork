e89e44c911c6fa3afaddc38783daa2e626c930dc
Vahe Ghazaryan <vahe.ghazaryan.99@mail.ru>
2022-03-29 20:46:38 +0400

    feat(Billable): created Billable component

--- src/components/WebWork.js
+++ src/components/WebWork.js
@@ -9,38 +9,45 @@ import {
   WebWorkLogo,
 } from "./Menu/NavBar";
 
+import Billable from "./Menu/Body/Billable";
+
 export default function WebWork() {
   return (
     <div>
-      <nav>
-        <header className="h-40">
-          <div>
-            <ul className="flex justify-center  mt-4 text-gray-700">
-              <WebWorkLogo />{" "}
-              <li className="mt-2 text-base font-serif">
-                <Home />
-              </li>
-              <li className="mt-2 text-base font-serif">
-                <Features />
-              </li>
-              <li className="mt-2 text-base font-serif">
-                <TimeTracking />
-              </li>
-              <li className="mt-2 text-base font-serif">
-                <Price />
-              </li>
-              <li className="mt-2 text-base font-serif">
-                <Download />
-              </li>
-              <li className="mt-2 text-base font-serif">
-                <Login />
-              </li>
-              <GetStarted />
-            </ul>
-          </div>
-        </header>
-      </nav>
-      <div></div>
+      <div>
+        <nav>
+          <header className="h-40">
+            <div>
+              <ul className="flex justify-center  mt-4 text-gray-700">
+                <WebWorkLogo />{" "}
+                <li className="mt-2 text-base font-serif">
+                  <Home />
+                </li>
+                <li className="mt-2 text-base font-serif">
+                  <Features />
+                </li>
+                <li className="mt-2 text-base font-serif">
+                  <TimeTracking />
+                </li>
+                <li className="mt-2 text-base font-serif">
+                  <Price />
+                </li>
+                <li className="mt-2 text-base font-serif">
+                  <Download />
+                </li>
+                <li className="mt-2 text-base font-serif">
+                  <Login />
+                </li>
+                <GetStarted />
+              </ul>
+            </div>
+          </header>
+        </nav>
+      </div>
+
+      <div>
+        <Billable />
+      </div>
     </div>
   );
 }
